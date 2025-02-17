2c9f304387ded95dd2d6997b4ad991605342ffad
halilozan <halilfurkanozan@hotmail.com>
2025-02-17 12:36:32 +0300

    profile sync

--- videoGame/src/main/java/com/springboot/videogame/controller/ProfileController.java
+++ videoGame/src/main/java/com/springboot/videogame/controller/ProfileController.java
@@ -8,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -39,6 +42,26 @@ public class ProfileController {
 
         return "profile"; // Profil sayfasını döndür
     }
+
+
+    @PostMapping("/profile/remove")
+    public String removeFavorite(@RequestParam Integer userId, @RequestParam Long gameId, RedirectAttributes redirectAttributes) {
+        userFavoriteGameService.removeFavoriteGame(userId, gameId);
+
+        // Kullanıcıyı favoriler sayfasına yönlendir
+        redirectAttributes.addFlashAttribute("message", "Game removed from favorites!");
+        return "redirect:/profile";
+    }
+
+    @PostMapping("/profile/add")
+    public String addFavoriteGame(@RequestParam Integer userId, @RequestParam Long gameId, RedirectAttributes redirectAttributes) {
+        userFavoriteGameService.addFavoriteGame(userId, gameId);
+
+        // Kullanıcıyı favoriler sayfasına yönlendir
+        redirectAttributes.addFlashAttribute("message", "Game added to favorites!");
+        return "redirect:/profile";
+    }
+
 
 
 }
